878591c24ffb55b0b6dc663e5fc90b0478540fa0
fiveofeight <hynste@gmail.com>
2012-09-03 19:23:29 -0400

    Added custom scripts for Halls of Reflection

--- src/server/scripts/Northrend/FrozenHalls/HallsOfReflection/boss_falric.cpp
+++ src/server/scripts/Northrend/FrozenHalls/HallsOfReflection/boss_falric.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -15,8 +15,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "ScriptMgr.h"
-#include "ScriptedCreature.h"
+#include "ScriptPCH.h"
 #include "halls_of_reflection.h"
 
 enum Yells
