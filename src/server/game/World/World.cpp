d91236724e0bc5e283bbaf72c5fa7133d32450f4
fiveofeight <hynste@gmail.com>
2012-09-17 16:51:43 -0400

    Added transmogrification (Thanks to Rochet2).

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1386,6 +1386,9 @@ void World::SetInitialWorldSettings()
     sLog->outInfo(LOG_FILTER_SERVER_LOADING, "Loading Item set names...");                // must be after LoadItemPrototypes
     sObjectMgr->LoadItemSetNames();
 
+    sLog->outString("Loading Transmogrifications...");           // custom must be after LoadItemTemplates
+    sObjectMgr->LoadTransmogrifications();
+
     sLog->outInfo(LOG_FILTER_SERVER_LOADING, "Loading Creature Model Based Info Data...");
     sObjectMgr->LoadCreatureModelInfo();
 
