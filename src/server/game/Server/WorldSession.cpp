753c5a37da5908b9fcea044add50ae3a6713a2fe
fiveofeight <hynste@gmail.com>
2012-10-05 12:55:24 -0400

    Players should no longer be removed from a group if they DC.

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -518,14 +518,6 @@ void WorldSession::LogoutPlayer(bool Save)
         ///- Leave all channels before player delete...
         _player->CleanupChannels();
 
-        ///- If the player is in a group (or invited), remove him. If the group if then only 1 person, disband the group.
-        _player->UninviteFromGroup();
-
-        // remove player from the group if he is:
-        // a) in group; b) not in raid group; c) logging out normally (not being kicked or disconnected)
-        if (_player->GetGroup() && !_player->GetGroup()->isRaidGroup() && m_Socket)
-            _player->RemoveFromGroup();
-
         //! Send update to group and reset stored max enchanting level
         if (_player->GetGroup())
         {
