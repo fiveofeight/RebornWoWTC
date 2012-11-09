8a05650719d11042694909a7146be3e958fa40b4
fiveofeight <hynste@gmail.com>
2012-11-09 18:07:14 -0500

    Reverted some stuff

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -2862,7 +2862,6 @@ void AuraEffect::HandleAuraAllowFlight(AuraApplication const* aurApp, uint8 mode
     if (!apply)
     {
         target->RemoveUnitMovementFlag(MOVEMENTFLAG_MASK_MOVING_FLY);
-        target->GetMotionMaster()->MoveFall();
         target->m_movementInfo.SetFallTime(0);
     }
 
